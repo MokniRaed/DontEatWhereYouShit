c62ca43ad577f183f2187cecbaef0813321ef6cd
meriambs <bensalah.meriem0@gmail.com>
2022-10-21 20:06:45 +0100

    adding controllers and routes pages

---
+++ backend/routes/joboffer.js
@@ -0,0 +1,3 @@
+const express = require('express')
+
+const jobOfferRouter = express.Router()
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
+
 const express = require('express')
 
 const cors = require('cors');
+
 const connectDb = require('./config/connection');
 
 const app = express()
 
-const Port = 4007;
-connectDb()
-require('dotenv').config()
- app.use(cors())
+// const Port = 4007;
 
- app.listen(Port,(err)=>{
-    err?console.log(err):console.log(`server is running on port ${Port}`)
+connectDb()
+
+require('dotenv').config()
+
+ app.use(cors())
+ 
+ app.listen(process.env.PORT,(err)=>{
+    err?console.log(err):console.log(`server is running on port ${process.env.PORT}`)
  })
