cc745b0de013b10397d22b33f240f92aa6ffd5de
meriambs <bensalah.meriem0@gmail.com>
2022-11-04 03:42:59 +0100

    register.js

--- backend/models/userRegister.js
+++ backend/models/userRegister.js
@@ -24,6 +24,90 @@ let userRegisterSchema = new mongoose.Schema({
      
    },
    phoneNumber:Number,
+   civilité:{
+    type:String,
+
+},
+
+    Gouvernorat:{
+        type:String,
+        required:true
+    },
+    LinkedIn:{
+        type:String,
+    },
+
+    website:{
+      type:String,
+  },
+  ExperienceExperience:[
+      {
+          Entreprise:{
+              type:String,
+              required:true
+          },
+          Poste:{
+              type:String,
+              required:true
+          },
+          DateDebut:{
+              type:String,
+              required:true
+          },
+          DateFin:{
+              type:String,
+              required:true
+          },
+          Description:{
+              type:String,
+              required:true
+          }}
+  ],
+  Formation:[
+      {
+          Diplome:{
+              type:String,
+              required:true
+          },
+          Ecole:{
+              type:String,
+              required:true
+          },
+          DateDebut:{
+              type:String,
+              required:true
+          },
+          DateFin:{
+              type:String,
+              required:true
+          },
+          Description:{
+              type:String,
+              required:true
+          }
+      }],
+      Competence:[
+          {
+              Nom:{
+                  type:String,
+                  required:true
+              },
+              Niveau:{
+                  type:String,
+                  required:true
+              }
+          }],
+          Langue:[
+              {
+                  Nom:{
+                      type:String,
+                      required:true
+                  },
+                  Niveau:{
+                      type:String,
+                      required:true
+                  }}]
+          
 });
 
 module.exports  = mongoose.model("userRegister", userRegisterSchema);
